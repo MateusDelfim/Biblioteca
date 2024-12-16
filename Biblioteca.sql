e2b8d0f80e857dd836ce8d105991eb515f0a0a88
Dinétria Lemos Camilo <131454212+lemos16@users.noreply.github.com>
2024-12-16 19:45:36 +0100

    Add files via upload
    Actualização ultima sprint

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -300,3 +300,127 @@ INNER JOIN Livros L ON LT.id_livro = L.id_livro
 INNER JOIN Funcionarios F ON LT.id_funcionario = F.id_funcionario
 WHERE LT.status = 'Concluída';
 
+-- CRIAÇÃO DE PROCESSOS ARMAZENADOS
+
+DELIMITER //
+
+CREATE PROCEDURE InserirLivro(
+    IN titulo VARCHAR(255),
+    IN autor VARCHAR(255),
+    IN ano_publicacao YEAR,
+    IN quantidade INT,
+    IN isbn VARCHAR(20),
+    IN id_categoria INT,
+    IN id_editora INT
+)
+BEGIN
+    INSERT INTO Livros (titulo, autor, ano_publicacao, quantidade, isbn, id_categoria, id_editora)
+    VALUES (titulo, autor, ano_publicacao, quantidade, isbn, id_categoria, id_editora);
+END //
+
+DELIMITER ;
+
+CALL InserirLivro(
+    'Doze semanas para mudar uma vida', 
+    'Augusto Cury', 
+    2012, 
+    7, 
+    '978-989-678-052-2', 
+    10, 
+    1
+);
+
+DELIMITER //
+
+DELIMITER //
+
+CREATE PROCEDURE InserirEmprestimo(
+    IN id_livro INT,
+    IN id_leitor INT,
+    IN id_funcionario INT,
+    IN data_emprestimo DATE,
+    IN data_devolucao DATE,
+    IN status ENUM('Emprestado', 'Devolvido', 'Atrasado')
+)
+BEGIN
+    INSERT INTO Emprestimo (id_livro, id_leitor, id_funcionario, data_emprestimo, data_devolucao, status)
+    VALUES (id_livro, id_leitor, id_funcionario, data_emprestimo, data_devolucao, status);
+END //
+
+DELIMITER ;
+
+CALL InserirEmprestimo(3, 2, 1, '2024-11-30', '2024-12-15', 'Emprestado');
+
+-- Criação de Views
+
+CREATE VIEW Leituras_Por_Leitor AS
+SELECT 
+    LT.nome AS Leitor,
+    L.titulo AS Livro,
+    LD.data_inicio,
+    LD.data_fim,
+    LD.status AS StatusLeitura
+FROM 
+    Leitura LD
+JOIN 
+    Livros L ON LD.id_livro = L.id_livro
+JOIN 
+    Leitores LT ON LD.id_leitor = LT.id_leitor;
+    
+    
+CREATE VIEW Livros_Categorias AS
+SELECT 
+    L.id_livro,
+    L.titulo AS Livro,
+    C.nome AS Categoria
+FROM 
+    Livros L
+JOIN 
+    Categoria C ON L.id_categoria = C.id_categoria;
+
+
+-- Consultas Views 
+
+SELECT * FROM Leituras_Por_Leitor;
+
+SELECT * FROM Livros_Categorias;
+
+-- Consultas com Order By
+
+-- Consulta por ordem alfabetica livros
+SELECT id_livro, titulo, autor, ano_publicacao, quantidade, isbn
+FROM Livros
+ORDER BY titulo ASC;
+
+-- consulta por ordem alfabetica leitores
+SELECT id_leitor, nome, email, telefone, endereco, data_cadastro
+FROM Leitores
+ORDER BY nome ASC;
+
+-- Consultas com like
+-- Buscar livros cujo título contém a palavra 'Vida'
+SELECT id_livro, titulo, autor, ano_publicacao, quantidade, isbn
+FROM Livros
+WHERE titulo LIKE '%vida%';
+
+-- Buscar leitores cujo nome comece com "M" 
+SELECT id_leitor, nome
+FROM Leitores
+WHERE nome LIKE 'D%' ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
